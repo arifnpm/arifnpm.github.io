3b96c29034f9039c09c6ff27e170cae200e828c4
arifnpm <arif.masyur@candelalabs.io>
2020-04-09 23:44:02 +0700

    add file listing

---
+++ files/react_class.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+export class {{name}} extends React.Component<any, any> {
+	constructor(props: any) {
+		super(props);
+		this.state = {
+		};
+	}
+
+	componentDidMount() {
+	}
+	
+	render() {
+		return (
+			<div>
+			</div>
+		)
+	}
+}
